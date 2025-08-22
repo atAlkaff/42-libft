695ad0dddd511a858ac97bc2d7cd031c46fa1a23
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-08-22 22:50:57 +0400

    implemented ft_bzero

---
+++ src/ft_bzero.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 22:49:30 by aalkaff           #+#    #+#             */
+/*   Updated: 2025/08/22 22:50:28 by aalkaff          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdint.h> // uintptr_t
+
+void	ft_bzero(void *s, size_t n)
+{
+	char	*ptr;
+	size_t	*wptr;
+
+	ptr = s;
+	while ((uintptr_t)ptr & (sizeof(size_t) - 1) && n)
+	{
+		*ptr++ = 0;
+		n--;
+	}
+	wptr = ptr;
+	while (n >= sizeof(size_t))
+	{
+		*wptr++ = 0;
+		n -= sizeof(size_t);
+	}
+	ptr = wptr;
+	while (n--)
+		*ptr++ = 0;
+	return (s);
+}
