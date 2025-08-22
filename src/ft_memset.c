0b1a4e42fae000e730dd4110515a95c383abafd9
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-08-22 22:49:06 +0400

    implemented ft_memset

---
+++ src/ft_memset.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/22 22:26:37 by aalkaff           #+#    #+#             */
+/*   Updated: 2025/08/22 22:45:10 by aalkaff          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdint.h> // uintptr_t
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	char	*ptr;
+	size_t	*wptr;
+	size_t	mask;
+
+	ptr = s;
+	while ((uintptr_t)ptr & (sizeof(size_t) - 1) && n)
+	{
+		*ptr++ = (unsigned char) c;
+		n--;
+	}
+	wptr = ptr;
+	mask = (unsigned char) c;
+	mask |= mask << 8;
+	mask |= mask << 16;
+	mask |= mask << 32;
+	while (n >= sizeof(size_t))
+	{
+		*wptr++ = mask;
+		n -= sizeof(size_t);
+	}
+	ptr = wptr;
+	while (n--)
+		*ptr++ = (unsigned char) c;
+	return (s);
+}
