412fdc1e06e38c9b5493ba919cba1c32d431281e
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2025-08-22 22:40:48 +0400

    norm

--- src/ft_strlen.c
+++ src/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/22 22:24:32 by aalkaff           #+#    #+#             */
-/*   Updated: 2025/08/22 22:26:11 by aalkaff          ###   ########.fr       */
+/*   Updated: 2025/08/22 22:40:31 by aalkaff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 size_t	ft_strlen(const char *s)
 {
-	const char *begin;
+	const char	*begin;
 
 	begin = s;
 	while (*s)
