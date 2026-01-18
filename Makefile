b5e351cb1076441a43d07e97a2fcd89c77547bdf
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2026-01-18 20:22:15 +0400

    Isolated ctype functions

--- Makefile
+++ Makefile
@@ -10,9 +10,9 @@ LIB = ranlib
 RM = rm -f
 
 # Sources
-SRC_FILES = ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
+SRC_FILES = ctype/ctype_class.c ctype/ctype_convert.c \
 	ft_strlen.c ft_strnlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c \
-	ft_strlcpy.c ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c \
+	ft_strlcpy.c ft_strlcat.c ft_strchr.c \
 	ft_strrchr.c ft_strncmp.c ft_memchr.c ft_memcmp.c ft_strnstr.c \
 	ft_atoi.c ft_calloc.c ft_strdup.c ft_substr.c ft_strjoin.c ft_strtrim.c \
 	ft_split.c ft_itoa.c ft_strmapi.c ft_striteri.c ft_putchar_fd.c \
--- src/ctype/ctype_class.c
+++ src/ctype/ctype_class.c
@@ -1,18 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isdigit.c                                       :+:      :+:    :+:   */
+/*   ctype_class.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/28 20:27:40 by aalkaff           #+#    #+#             */
-/*   Updated: 2025/10/28 20:28:22 by aalkaff          ###   ########.fr       */
+/*   Created: 2026/01/18 20:13:00 by aalkaff           #+#    #+#             */
+/*   Updated: 2026/01/18 20:16:30 by aalkaff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_isalnum(int c)
+{
+	return ((unsigned)((c | 32) - 'a') < 26
+		|| (unsigned)(c - '0') < 10);
+}
+
+int	ft_isalpha(int c)
+{
+	return ((unsigned)((c | 32) - 'a') < 26);
+}
+
+int	ft_isascii(int c)
+{
+	return ((unsigned)c <= 127);
+}
+
 int	ft_isdigit(int c)
 {
 	return ((unsigned)(c - '0') < 10);
+}
+
+int	ft_isprint(int c)
+{
+	return ((unsigned)(c - 32) < 95);
 }
