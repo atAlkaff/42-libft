92f5648510b4d7824d4437fa27c0f7dcab012c0c
Ahmed Alkaff <ahmedalkaff2006@yahoo.com>
2026-01-30 23:43:15 +0400

    moved all core requirements into categories

--- src/io/output/ft_putstr_fd.c
+++ src/io/output/ft_putstr_fd.c
@@ -6,7 +6,7 @@
 /*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/29 22:06:18 by aalkaff           #+#    #+#             */
-/*   Updated: 2025/10/29 22:08:02 by aalkaff          ###   ########.fr       */
+/*   Updated: 2026/01/30 23:42:20 by aalkaff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,12 @@ void	ft_putstr_fd(char *s, int fd)
 	if (!s)
 		return ;
 	write(fd, s, ft_strlen(s));
+}
+
+void	ft_putendl_fd(char *s, int fd)
+{
+	if (!s)
+		return ;
+	write(fd, s, ft_strlen(s));
+	write(fd, "\n", 1);
 }
