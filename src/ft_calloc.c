56e07f3eba84d1ca7089b837b08fb0f5410a0142
Ahmed Alkaff <aalkaff@lab3r3s5.42abudhabi.ae>
2026-01-31 18:59:57 +0400

    added errno to ft_calloc

--- src/ft_calloc.c
+++ src/ft_calloc.c
@@ -6,13 +6,14 @@
 /*   By: aalkaff <aalkaff@student.42abudhabi.ae>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 21:31:55 by aalkaff           #+#    #+#             */
-/*   Updated: 2025/10/29 22:52:34 by aalkaff          ###   ########.fr       */
+/*   Updated: 2026/01/31 18:59:28 by aalkaff          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>	// malloc
 #include <stdint.h>	// SIZE_MAX
+#include <errno.h>	// ENOMEM
 
 void	*ft_calloc(size_t nelem, size_t elsize)
 {
@@ -20,13 +21,19 @@ void	*ft_calloc(size_t nelem, size_t elsize)
 	size_t	len;
 
 	if (elsize != 0 && nelem > SIZE_MAX / elsize)
+	{
+		ft_seterrno(ENOMEM);
 		return (NULL);
+	}
 	len = nelem * elsize;
 	if (len == 0)
 		len = 1;
 	ptr = malloc(len);
 	if (!ptr)
+	{
+		ft_seterrno(ENOMEM);
 		return (NULL);
-	ft_bzero(ptr, len);
+	}
+	ft_memset(ptr, 0, len);
 	return (ptr);
 }
